da172f0ee2076445d5dbf2ac3e57d0761017aff8
Sparsh Anand <56508431+0xSN1PE@users.noreply.github.com>
2020-10-08 19:38:07 +0530

    Update rev-pwn-tools.sh

--- rev-pwn-tools.sh
+++ rev-pwn-tools.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-#MY SYSTEM BREAKS TOO OFTEN SO THIS IS A SMALL SCRIPT TO SETUP ALL MY STUFF AGAIN
+#REQUIRED PACKAGES
 
 apt-get update
 apt-get install python3-pip -y
 apt-get install git -y
 apt-get install gdb -y
 apt-get install gcc-multilib -y
-
-
-#GEF SETUP
-
-wget -q -O- https://github.com/hugsy/gef/raw/master/scripts/gef.sh | sh
-pip3 install capstone unicorn keystone-engine ropper
-wget -q -O- https://github.com/hugsy/gef/raw/master/scripts/gef-extras.sh | sh
-pip3 install -r ~/gef-extras/requirements.txt
 
 #PWNTOOLS SETUP
 
